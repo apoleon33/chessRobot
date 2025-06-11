6381130c356f3ca951177039f7464e09fda5de92
Simon Dumesny <simondumesny@gmail.com>
2025-06-11 13:28:47 +0000

    conf update

--- conf.py
+++ conf.py
@@ -5,3 +5,10 @@ ROBOT_IP = "192.168.12.126"
 GRIPPER_IP = "192.168.12.244"
 GRIPPER_PORT = 502
 GRIPPER_MODEL = "rg6"
+
+# misc
+DEBUG = True
+def log(text: str) -> None:
+    """ N'affiche le message dans la console que si on est en mode debug"""
+    if DEBUG:
+        print(text)
