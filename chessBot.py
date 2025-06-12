a1cd4a9964353151e9209c151facba9cf402c6f0
Simon Dumesny <simondumesny@gmail.com>
2025-06-12 09:05:39 +0000

    formatting

--- chessBot.py
+++ chessBot.py
@@ -3,6 +3,7 @@ import time
 import rtde_control
 
 from conf import *
+from coordinate import Coordinate
 from libs.gripper import RG
 
 
@@ -10,6 +11,7 @@ class ChessBot:
     gripper: RG
     robot: rtde_control.RTDEControlInterface
 
+    coordinate: Coordinate
     # Les coordonnées pour placer la pince au dessus de la case A1
     A1_COORDINATE: list[float] = [0.505, 0.049, -0.05, 2.32, -2.11, 0.0]
 
@@ -18,8 +20,22 @@ class ChessBot:
     # hauteur nécessaire pour prendre une pièce
     PIECE_HEIGHT: int = -0.225
 
-    def __init__(self, gripper_model: str = GRIPPER_MODEL, gripper_ip=GRIPPER_IP, gripper_port=GRIPPER_PORT,
-                 robot_ip=ROBOT_IP):
+    speed: float = 0.5
+    acceleration: float = 0.3
+
+    def __init__(self,
+                 gripper_model: str = GRIPPER_MODEL,
+                 gripper_ip=GRIPPER_IP,
+                 gripper_port=GRIPPER_PORT,
+                 robot_ip=ROBOT_IP
+                 ):
+        """
+        Créé un objet ``ChessBot`` à partir des informations sur la pince et le robot données en argument.
+        :param gripper_model:
+        :param gripper_ip:
+        :param gripper_port:
+        :param robot_ip:
+        """
         self.gripper = RG(
             gripper=gripper_model,
             ip=gripper_ip,
@@ -28,12 +44,15 @@ class ChessBot:
 
         self.robot = rtde_control.RTDEControlInterface(robot_ip)
 
+        self.coordinate = Coordinate()  # pas synchronisé avec le robot à ce moment là du code
+
     def start(self):
         """ Positionne le robot en A1 pour débuter la partie"""
 
         log("Lancement du robot, déplacement vers la case A1 et ouverture de la pince")
 
-        self.robot.moveL(self.A1_COORDINATE, 0.5, 0.3) # à voir pour le 0.5 et 0.3
+        self.robot.moveL(self.A1_COORDINATE, self.speed, self.acceleration)
+        self.coordinate = Coordinate(self.A1_COORDINATE)  # synchronisé
         self.open_gripper()
 
         log("Fin de la procédure 'start'")
@@ -41,7 +60,7 @@ class ChessBot:
     def open_gripper(self) -> None:
         """
         Ouvre la pince de la bonne largeur pour attraper une pièce.
-        :return: None
+        :return: ``None``
         """
         log(f"Ouverture de la pince d'une largeur {self.PIECE_WIDTH}mm")
         self.gripper.move_gripper(self.PIECE_WIDTH)
@@ -52,3 +71,9 @@ class ChessBot:
         self.gripper.close_gripper()
         while self.gripper.get_status()[0]:
             time.sleep(0.5)
+
+    def close(self):
+        """ Déconnecte la pince et le robot """
+        log("Closing connection...")
+        self.gripper.close_connection()
+        self.robot.disconnect()
--- coordinate.py
+++ coordinate.py
@@ -1,7 +1,7 @@
 from conf import log
 
-
 class Coordinate:
+    # les coordonnées du robot lorsqu'il est à (0,0,0) sur l'échiquier
     initalCoordinate: list[float]
     __x: float
     __y: float
@@ -12,9 +12,18 @@ class Coordinate:
     rz: float
 
     checkerXCoordinate: list[str] = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    caseSize: float = 0.0425
 
-    def __init__(self, coord: list[float]) -> None:
+    # TODO: en faire une variable par axe, et affiner la précision
+    caseSize: float = 0.0425 # largeur/hauteur d'une case (en m)
+
+    def __init__(self, coord=None) -> None:
+        """
+        Créé un objet ``Coordinate`` à partie des coordonnées initiales passée en argument.
+        :param coord: une liste de ``float`` qui indique la position initiale de la forme ``[x,y,z,rx,ry,rz]``.
+        """
+        if coord is None: # pas direct en argument pour éviter des problèmes de mutabilité
+            coord = [0, 0, 0, 0, 0, 0]
+
         self.initalCoordinate = coord
         self.__x = coord[0]
         self.__y = coord[1]
@@ -26,17 +35,17 @@ class Coordinate:
 
     @property
     def case(self) -> str:
-        """ Renvoie la case actuelle sur le plateau"""
-        return f"{self.checkerXCoordinate[int((self.__x - self.initalCoordinate[0]) / self.caseSize)]}{int((self.__y - self.initalCoordinate[1]) / self.caseSize) + 1}"
+        """ Renvoie la case actuelle sur le plateau, par exemple `a1` """
+        return f"{self.checkerXCoordinate[int((self.__y - self.initalCoordinate[1]) / self.caseSize)]}{int((self.__x - self.initalCoordinate[0]) / self.caseSize) + 1}"
 
     @property
     def x(self) -> float:
         return self.__x
 
-    def changeX(self, newX:int) -> None:
+    def changeX(self, newX: int) -> None:
         self.__x += newX * self.caseSize
 
-    def changeY(self, newY:int) -> None:
+    def changeY(self, newY: int) -> None:
         self.__y -= newY * self.caseSize
 
     @property
@@ -45,4 +54,5 @@ class Coordinate:
 
     @property
     def robotCoordinate(self) -> list[float]:
+        """ Formate les coordonnées pour pouvoir directement les utiliser dans une commande de type ``moveL`` """
         return [self.__x, self.__y, self.z, self.rx, self.ry, self.rz]
