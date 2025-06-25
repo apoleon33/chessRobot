837374bc0cd8cd74bad0f50dccb5c6e1df412469
Simon Dumesny <simondumesny@gmail.com>
2025-06-25 22:15:15 +0000

    main.py & setup_robot

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import time
-
-from chessBot import ChessBot
-from chessEngine import ChessEngine
 from conf import STOCKFISH_PATH_LINUX
+from game import Game
 
-engine = ChessEngine(STOCKFISH_PATH_LINUX, depth=20)
-engine.stockfish.set_elo_rating(2000)
-print(engine)
-while True:
-    time.sleep(0.5)
-    engine.play()
-    print(engine)
-
+if __name__ == "__main__":
+    game = Game(STOCKFISH_PATH_LINUX)
--- setup_robot.py
+++ setup_robot.py
@@ -5,7 +5,18 @@ from conf import STOCKFISH_PATH_LINUX
 def setupRobot():
     engine = ChessEngine(STOCKFISH_PATH_LINUX)
     engine.robot.start()
-    engine.robot.close()
+    input("attente de la prise de la pièce...")
+    engine.robot.close_gripper()
+
+    print("déplacement en a1")
+    engine.robot.coordinate.z = -0.220
+    engine.robot.goToCase("a1")
+
+
+
+    input("attente vers déplacement en a8")
+    engine.robot.goToCase("a8")
+    input("attente")
 
 if __name__ == '__main__':
     setupRobot()
